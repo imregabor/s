8bf7ee3f7a2bb08daa57fe0819ae3436dc82c1ee
Gabor Imre <imregabor@gmail.com>
2023-01-29 14:30:56 +0000

    write-smart-info.sh added

---
+++ write-smart-info.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#
+# Write smart info of the disk 
+#
+# Note that device identification is broken for various cases
+#
+
+
+#see https://stackoverflow.com/questions/18215973/how-to-check-if-running-as-root-in-a-bash-script
+if [ "$EUID" -ne 0 ]; then
+  echo "Error! Run as root, exiting"
+  exit -1
+fi
+
+
+FILENAME='./smart-'$(date -Is)'.txt'
+FMO=$(findmnt -fn .)
+PART=$(echo "$FMO" | cut -d' ' -f2)
+DEV=$(echo "$PART" | sed -e "s/^\(.*\/sd.\)./\1/")
+
+echo "Write SMART info to: ${FILENAME}"
+echo "Findmnt info:        ${FMO}"
+echo "Partition:           ${PART}"
+echo "Device:              ${DEV}"
+
+smartctl -a "$DEV" > "${FILENAME}"
+
+echo "done."
