7e863b9368da229f52c07bbab7d1425695a67439
Gabor Imre <imregabor@gmail.com>
2023-02-02 18:59:28 +0000

    Clarify message

--- write-smart-info.sh
+++ write-smart-info.sh
@@ -8,7 +8,7 @@
 
 #see https://stackoverflow.com/questions/18215973/how-to-check-if-running-as-root-in-a-bash-script
 if [ "$EUID" -ne 0 ]; then
-  echo "Error! Run as root, exiting"
+  echo "Error! Must run as root, exiting"
   exit -1
 fi
 
