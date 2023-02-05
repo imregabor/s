7ec70d8c0bd3efd7774d005160e4bdffb1fd869c
Gabor Imre <imregabor@gmail.com>
2023-02-05 14:12:17 +0000

    Skip du when there is no missing status

--- check-for-allsha.sh
+++ check-for-allsha.sh
@@ -6,6 +6,7 @@ echo "Presence of checkus file"
 echo
 echo
 
+HASMISSING=false
 while read line ; do
   STAT="?"
   if [ -f "${line}/all.sha1" ] ; then
@@ -14,22 +15,25 @@ while read line ; do
     STAT="IN PROGRESS  "
   else
     STAT="** MISSING **"
+    HASMISSING=true
   fi
   echo "$STAT $line"
 done < <( find -maxdepth 1 -mindepth 1 -type d)
 
-echo
-echo "Size of unchecked directories"
-echo
+if "$HASMISSING" ; then
+    echo
+    echo "Size of unchecked directories"
+    echo
 
-while read line ; do
-  if [ -f "${line}/all.sha1" ] || [ -f "${line}/all.sha1-inprogress" ]; then
-    continue
-  fi
-  echo "$line:"
-  echo "  "$(du -hs "$line")
-  echo
-done < <( find -maxdepth 1 -mindepth 1 -type d)
+    while read line ; do
+      if [ -f "${line}/all.sha1" ] || [ -f "${line}/all.sha1-inprogress" ]; then
+        continue
+      fi
+      echo "$line:"
+      echo "  "$(du -hs "$line")
+      echo
+    done < <( find -maxdepth 1 -mindepth 1 -type d)
+fi
 
 echo
 echo "all done."
