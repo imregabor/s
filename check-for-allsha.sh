6e5a9cc18c99cf1a4037fcca0782f1303c1f85e7
Gabor Imre <imregabor@gmail.com>
2023-02-05 00:39:38 +0000

    Invoke du in not checkusmmed dirs

--- check-for-allsha.sh
+++ check-for-allsha.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo
+echo
+echo "Presence of checkus file"
+echo
+echo
+
 while read line ; do
   STAT="?"
   if [ -f "${line}/all.sha1" ] ; then
@@ -9,8 +15,22 @@ while read line ; do
   else
     STAT="** MISSING **"
   fi
-
   echo "$STAT $line"
-
-
 done < <( find -maxdepth 1 -mindepth 1 -type d)
+
+echo
+echo "Size of unchecked directories"
+echo
+
+while read line ; do
+  if [ -f "${line}/all.sha1" ] || [ -f "${line}/all.sha1-inprogress" ]; then
+    continue
+  fi
+  echo "$line:"
+  echo "  "$(du -hs "$line")
+  echo
+done < <( find -maxdepth 1 -mindepth 1 -type d)
+
+echo
+echo "all done."
+echo
