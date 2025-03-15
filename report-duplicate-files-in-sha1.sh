2b77da66190e4bc37f5bb196fbeac7d650d00c14
Gabor Imre <imregabor@gmail.com>
2025-03-15 21:28:57 +0100

    Write checksum file for hits

--- report-duplicate-files-in-sha1.sh
+++ report-duplicate-files-in-sha1.sh
@@ -3,19 +3,18 @@
 # Find duplicates based on checksums file
 #
 
-
 if [ "$#" -lt 1 ] || [ "$#" -gt 2 ]; then
   echo "Usage: $0 <checksum_file1> [checksum_file2]"
   exit 1
 fi
 
+checksum_ext="${1##*.}"
 checksum_file1="$1"
 checksum_file2="$2"
 checksum_file1_abs=$(readlink -m "$1")
 checksum_file2_abs=$(readlink -m "$2")
 checksum_file1_dir=$(dirname "$checksum_file1_abs")
 checksum_file2_dir=$(dirname "$checksum_file2_abs")
-
 
 if [ ! -f "$checksum_file1" ]; then
   echo "Error: File 1 '$checksum_file1' not found."
@@ -29,8 +28,6 @@ if [ ! -z "$checksum_file2" ]; then
   fi
 fi
 
-
-
 OD="./checksum-duplicate-report-$(date -u +%Y%m%d-%H%M%S)"
 LOGFILE="$OD/report.log"
 echo "Output directory: $OD"
@@ -41,9 +38,10 @@ log() {
   echo "$1" | tee -a "$LOGFILE"
 }
 
-
 log
 log "================================================================"
+log
+log "Ext:           $checksum_ext"
 log
 log "Checksum 1:    $checksum_file1"
 log "               $checksum_file1_abs"
@@ -59,6 +57,9 @@ log "================================================================"
 log
 
 if [ -z "$checksum_file2" ]; then
+  log "1 File mode, find duplicates"
+  log
+
   FILTERED_CHECKSUMS="$OD/filtered-checksums"
   log "Looking for duplicate checksums, write results to $FILTERED_CHECKSUMS"
 
@@ -79,27 +80,53 @@ if [ -z "$checksum_file2" ]; then
     log
   done
 else
+  log "2 File mode, find checksums found in both"
+  log
   FILTERED_CHECKSUMS="$OD/filtered-checksums"
   log "Looking for duplicate checksums, write results to $FILTERED_CHECKSUMS"
   comm -12 <(awk '{print $1}' "$checksum_file1" | sort | uniq) <(awk '{print $1}' "$checksum_file2" | sort | uniq) > "$FILTERED_CHECKSUMS"
   DUPLICATE_COUNT=$(cat "$FILTERED_CHECKSUMS" | wc -l)
-  
 
-  log "  Done, duplicate checksum instances: $DUPLICATE_COUNT"
+  CCSF1="$OD/hits-in-1.$checksum_ext"
+  CCSF2="$OD/hits-in-2.$checksum_ext"
+  DS1="$OD/delete-hits-in-1.sh"
+  DS2="$OD/delete-hits-in-2.sh"
+
+  log "  Done, duplicate checksum instances:    $DUPLICATE_COUNT"
   log
-  
+  log "  Writing checksums for files hits in 1: $CCSF1"
+  log "  Writing checksums for files hits in 2: $CCSF2"
+  log
+  log "  Writing delete script for hits in 1:   $DS1"
+  log "  Writing delete script for hits in 2:   $DS2"
+  log
 
   I=0
+  echo -e '#!/bin/bash\n\n# This is dangerous!\nexit 1\n\n' > "$DS1"
+  echo -e '#!/bin/bash\n\n# This is dangerous!\nexit 1\n\n' > "$DS2"
 
-  # Two file mode: Compare checksums between both files
+
   cat "$FILTERED_CHECKSUMS" | while read checksum; do
     I=$(( I + 1 ))
     log "Matching checksum $I of $DUPLICATE_COUNT in both files: $checksum"
     log "  [$checksum_file1]"
-    dupes1=$(grep "^$checksum" "$checksum_file1" | sed -e 's/^/    /')
+
+    hits1=$(grep "^$checksum" "$checksum_file1")
+
+    echo "$hits1" >> "$CCSF1"
+    echo "$hits1" | sed -E 's/^[^ ]+ .(.*)$/rm "\1"/' >> "$DS1"
+
+    dupes1=$(echo "$hits1" | sed -e 's/^/    /')
+
     log "$dupes1"
     log "  [$checksum_file2]"
-    dupes2=$(grep "^$checksum" "$checksum_file2" | sed -e 's/^/    /')
+
+    hits2=$(grep "^$checksum" "$checksum_file2")
+
+    echo "$hits2" >> "$CCSF2"
+    echo "$hits2" | sed -E 's/^[^ ]+ .(.*)$/rm "\1"/' >> "$DS2"
+
+    dupes2=$(echo "$hits2" | sed -e 's/^/    /')
     log "$dupes2"
     log
   done
