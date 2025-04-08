3923178c02d2c1e30b7c117b5bd9481238b1e6b7
Gabor Imre <imregabor@gmail.com>
2025-04-09 01:58:27 +0200

    Separate fix add/delete options, fix counting, count individual file changes, write individual file logs

--- report-sha1-sync.sh
+++ report-sha1-sync.sh
@@ -26,14 +26,16 @@ usage() {
   echo "  all hits will be processed."
   echo
   echo "Options:"
-  echo "  -h, --help     Print this help and exit."
-  echo "  -f, --fix      Fix checksum file(s) in case of discrepancy."
+  echo "  -h,  --help         Print this help and exit."
+  echo "  -fn, --fix-new      Fix checksum file(s) in case of new file discrepancy - calculate checksum for new files."
+  echo "  -fd, --fix-deleted  Fix checksum file(s) in case of deleted file discrepancy - remove missing file from checksum."
   echo
   exit 1
 }
 
 # Default values
-FIX_MODE=false
+FIX_NEW=false
+FIX_DELETED=false
 TARGET=""
 
 # Argument parsing
@@ -42,8 +44,12 @@ while [[ $# -gt 0 ]]; do
     -h|--help)
       usage
       ;;
-    -f|--fix)
-      FIX_MODE=true
+    -fn|--fix-new)
+      FIX_NEW=true
+      shift
+      ;;
+    -fd|--fix-deleted)
+      FIX_DELETED=true
       shift
       ;;
     *)
@@ -76,6 +82,9 @@ fi
 # Output dir for report
 OD=$(readlink -m "./sha1-checksum-consistency-report-$(date -u +%Y%m%d-%H%M%S)")
 LOGFILE="$OD/report.log"
+INDIVIDUAL_MISSING_FILES_LIST="$OD/all-missing-files.txt"
+INDIVIDUAL_ADDED_FILES_LIST="$OD/all-new-files.txt"
+
 mkdir -p "$OD" || { echo "Error: Failed to create output report directory '$OD'." >&2; exit 1; }
 
 log() {
@@ -83,7 +92,7 @@ log() {
 }
 
 
-# Visited checksum file count
+# Procesed checksum file count
 CT=0
 # All checksum file count
 ALLCT=0
@@ -100,10 +109,16 @@ CT_ADDED=0
 # Checksum files involved in removal
 CT_REMOVED=0
 
+# Individual files missing but referenced in all checksums (might be duplicated)
+CT_TOTAL_MISSING_FILES=0
+
+# Number of file entries missing from checksums (might be duplicated)
+CT_TOAL_ADDED_FILES=0
+
 # All checksum files encountered
 ALLCHECKSUMFILES="$OD/all-checksum-files.txt"
 
-visit() {
+process_checksum_file() {
   local CHECKSUMFILE
   CHECKSUMFILE=$(readlink -m "$1")
 
@@ -132,8 +147,8 @@ visit() {
   log "= Original checksum file lists:    $OCFFL"
   log "= New file list:                   $NFL"
   log "= Removed file list:               $RFL"
-  if [ "$FIX_MODE" = true ]; then
-    log "= New checksum file:               $NCF"
+  if [ "$FIX_NEW" = true ] || [ "$FIX_DELETED" = true ]; then
+    log "= Updated checksum file:         $NCF"
   fi
   log "="
   log "=============================================================================================================="
@@ -164,12 +179,17 @@ visit() {
   CHANGE=false
 
   log
-  if [ "$FIX_MODE" = true ]; then
-    # Fixing
-    if [ -s "$RFL" ] ; then
-      CT_REMOVED=$((CT_REMOVED+1))
-      log "  Remove missing files from checksum. Removed file count: "$(wc -l < "$RFL")
 
+  if [ -s "$RFL" ] ; then
+    MISSING_FILES=$(wc -l < "$RFL")
+
+    CT_REMOVED=$((CT_REMOVED+1))
+    CT_TOTAL_MISSING_FILES=$((CT_TOTAL_MISSING_FILES+MISSING_FILES))
+    CHANGE=true
+    log "  Files referenced in checksum but not found (removed on FS) count: $MISSING_FILES"
+
+    if [ "$FIX_DELETED" = true ]; then
+      log "  Remove checksums for missing files"
       awk 'NR==FNR {
           remove[$0] = "x"
           next
@@ -185,64 +205,66 @@ visit() {
           path = substr($0, path_start + 2)
           if (!(path in remove)) print $0
         }' "$RFL" "$OCF" > "$NCF"
-
-      CHANGE=true
     else
-      log "  No files to remove"
+      log "  No removal from checksum will be made."
+    fi
+
+    log "  Missing files:"
+    while IFS= read -r REMOVEDFILE ; do
+      log "    > $REMOVEDFILE"
+      echo $(readlink -m "$REMOVEDFILE") >> "$INDIVIDUAL_MISSING_FILES_LIST"
+    done < "$RFL"
+  else
+    log "  No files removed from the FS"
+    if [ "$FIX_NEW" = true ] || [ "$FIX_DELETED" = true ]; then
+      # New checksum file is not written when no fix is requested
       cp "$OCF" "$NCF"
     fi
-    log
-    if [ -s "$NFL" ];  then
-      CT_ADDED=$((CT_ADDED+1))
-      log "  Calculate checksums for new files. New file count: "$(wc -l < "$NFL")
+  fi
+
+  if [ -s "$NFL" ];  then
+    ADDED_FILES=$(wc -l < "$NFL")
+
+    CT_ADDED=$((CT_ADDED+1))
+    CT_TOTAL_ADDED_FILES=$((CT_TOTAL_ADDED_FILES+ADDED_FILES))
+    CHANGE=true
+    log "  Added files found but missing from checksums (added on FS) count: $ADDED_FILES"
+
+    if [ "$FIX_NEW" = true ]; then
+      log "  Calculate checksums for added files"
       while IFS= read -r NEWFILE ; do
         log "    > $NEWFILE"
         sha1sum -b "$NEWFILE" >> "$NCF"
+        echo $(readlink -m "$NEWFILE") >> "$INDIVIDUAL_ADDED_FILES_LIST"
       done < "$NFL"
-      CHANGE=true
+    else
+      log "  No checksum calculation for new files will be done. Added files:"
+      while IFS= read -r NEWFILE ; do
+        log "    > $NEWFILE"
+        echo $(readlink -m "$NEWFILE") >> "$INDIVIDUAL_ADDED_FILES_LIST"
+      done < "$NFL"
     fi
-    log
-    if [ "$CHANGE" = true ]; then
-      CD_CHANGED=$((CD_CHANGED+1))
+  else
+    log "  No files added to the FS"
+  fi
+  log
+
+  if [ "$CHANGE" = true ]; then
+    CT_CHANGED=$((CT_CHANGED+1))
+
+    if [ "$FIX_NEW" = true ] || [ "$FIX_DELETED" = true ]; then
       log "  Archive and overwrite"
       cp -v "$CHECKSUMFILE" "$CHECKSUMFILE.sha1-backup-$(date -u +%Y%m%d-%H%M%S)" | sed -e 's/^/    /'
       cp -v "$NCF" "$CHECKSUMFILE" | sed -e 's/^/    /'
-    else
-      CD_UNCHANGED=$((CD_UNCHANGED+1))
-      log "  Checksum file is valid, no need to update"
     fi
+
   else
-    # Not fixing just logging
-    if [ -s "$RFL" ] ; then
-      CT_REMOVED=$((CT_REMOVED+1))
-      log "  There are missing files, no changes will be made, skip removal. Missing files:"
-      while IFS= read -r REMOVEDFILE ; do
-        log "    > $REMOVEDFILE"
-      done < "$RFL"
-      CHANGE=true
-    else
-      log "  No files to remove"
-    fi
-    log
-    if [ -s "$NFL" ];  then
-      CT_ADDED=$((CT_ADDED+1))
-      log  "  There are new files, no changes will be made, skip checksum calculation. New files:"
-      while IFS= read -r NEWFILE ; do
-        log "    > $NEWFILE"
-      done < "$NFL"
-      log
-      CHANGE=true
-    else
-      log "  No new files"
-    fi
-    if [ "$CHANGE" = true ]; then
-      CD_CHANGED=$((CD_CHANGED+1))
-    else
-      CD_UNCHANGED=$((CD_UNCHANGED+1))
-    fi
+    CT_UNCHANGED=$((CT_UNCHANGED+1))
 
+    if [ "$FIX_NEW" = true ] || [ "$FIX_DELETED" = true ]; then
+      log "  Checksum file is in sync with FS, no need to update"
+    fi
   fi
-
 
   log
   log
@@ -257,10 +279,11 @@ visit() {
   fi
   log
   log "  Valid checksum files so far: $CT_UNCHANGED, invalid: $CT_CHANGED, +: $CT_ADDED, -: $CT_REMOVED"
+  log "    Individual missing files referenced in checksums so far: $CT_TOTAL_MISSING_FILES"
+  log "    Infividual new files not referenced in checksums so far: $CT_TOTAL_ADDED_FILES"
   log
   log
   log
-
 }
 
 
@@ -269,12 +292,15 @@ log "***************************************************************************
 log "**"
 log "**  SHA1 checksums consistency report"
 log "**"
-log "** Current path:               $(pwd)"
-log "** Do fixes:                   $FIX_MODE"
-log "** Target:                     $TARGET"
-log "** Output directory:           $OD"
-log "** Logfile:                    $LOGFILE"
-log "** List of all checksum files: $ALLCHECKSUMFILES"
+log "** Current path:                     $(pwd)"
+log "** Do fix new files (calc checksum): $FIX_NEW"
+log "** Do fix deleted (remove checksum): $FIX_DELETEDS"
+log "** Target:                           $TARGET"
+log "** Output directory:                 $OD"
+log "** Logfile:                          $LOGFILE"
+log "** List of all checksum files:       $ALLCHECKSUMFILES"
+log "** List of new files:                $INDIVIDUAL_ADDED_FILES_LIST"
+log "** List of removed files:            $INDIVIDUAL_MISSING_FILES_LIST"
 log "**"
 log "**************************************************************************************************************"
 log "**************************************************************************************************************"
@@ -293,7 +319,7 @@ if [[ -f "$TARGET" ]]; then
 
   ALLCT=1
 
-  visit "$TARGET"
+  process_checksum_file "$TARGET"
 
 elif [[ -d "$TARGET" ]]; then
   log "Target is a directory, searching for 'all.sha' files in: $TARGET"
@@ -314,7 +340,7 @@ elif [[ -d "$TARGET" ]]; then
 
   while IFS= read -r CHECKSUMFILE ; do
     cd "$SRCDIR"
-    visit "$CHECKSUMFILE"
+    process_checksum_file "$CHECKSUMFILE"
   done < "$ALLCHECKSUMFILES"
 
 else
@@ -325,4 +351,6 @@ fi
 log "=============================================================================================================="
 log "All done."
 log "  Valid checksum files so far: $CT_UNCHANGED, invalid: $CT_CHANGED, +: $CT_ADDED, -: $CT_REMOVED"
+log "    Individual missing files referenced in checksums so far: $CT_TOTAL_MISSING_FILES"
+log "    Infividual new files not referenced in checksums so far: $CT_TOTAL_ADDED_FILES"
 log "=============================================================================================================="
