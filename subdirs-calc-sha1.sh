d2aed6c5af94a352fd7bfcfa01f188651a3e2b4f
Gabor Imre <imregabor@gmail.com>
2025-04-25 22:09:32 +0200

    Doc update

--- subdirs-calc-sha1.sh
+++ subdirs-calc-sha1.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-
-# Find all directories from the current location recursively
+#
+# Find all immediate child directories from the current location
+# and invoke sha1 calculation in all of them.
+#
+# Note that existing all.sha1 (or all.sha1-inprogress) wont be touched
+#
 find -maxdepth 1 -mindepth 1 -type d | while read -r dir; do
     (cd "$dir" && "$(dirname "$0")/calc-all-sha1.sh")
 done
