274f69fecfd0cc933114b03b43f2a14d8497c28a
Gabor Imre <imregabor@gmail.com>
2025-08-03 03:21:11 +0200

    Doc update, recognize/use disk ID

--- README.md
+++ README.md
@@ -2,3 +2,30 @@ Random scripts
 ==============
 
 Use at your own risk.
+
+Checksum manipulation
+---------------------
+
+### `calc-all-sha1.sh`
+
+Write `all.sha1` in the working directory for all files. When an argument is given `cd` into that directory
+for the traversal (output is written into the original working directory, paths will be relative to the specified
+dir.) Fails if checksum or intermediate scratch file is already present.
+
+### `continue-all-sha1.sh`
+
+Finish an interrupted `calc-all-sha1.sh` run / extend an already calculated `all.sha1` with freshly added files.
+Fails when bot an `all.sha1` and an intermediate scratch file `all.sha1-inprogress` are present. Wont recognize/remove
+already present checksums (from already existing `all.sha1` / `all.sha1-inprogress` files) for files no longer present.
+
+
+Listing
+-------
+
+
+### `write-full-file-list.sh`
+
+Write various deep listings from the current directory, useful for offline metadata processing/data inventory.
+When an empty file matching glob `___*___*___` is present it will be treated as `<DISK ID>`, the listings will be
+written into `./___listings___/<DISK_ID>/`
+
--- write-full-file-list.sh
+++ write-full-file-list.sh
@@ -1,18 +1,58 @@
 #!/bin/bash
 #
-# Write file list, recursive ls and collect all calculated sha1 checksums
+# Create multiple listings of the current directory:
 #
+#  - List of all files
+#  - Output of recursive ls
+#  - Union of SHA1 checksums
+#  - Directory size (in bytes) report for all dirs
+#  - List of git remotes for git repos
+#
+# When an empty file matching glob ___*___*___ is present it will be treated as
+# a <DISK ID>, write the listings into ./___listings___/<DISK_ID>/
 #
 
 # TS=$(date -Iseconds)
-TS=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
-LOF="./all-${TS}.filelist"
-SOF="./all-${TS}.sha1"
-LSR="./all-${TS}.lsr"
-DUF="./all-${TS}.du"
-GRS="./all-${TS}.gitrepos"
+TS=$(date -u +"%Y-%m-%d___%H-%M-%S")
 
-echo "Create full file list to $LOF"
+BASEDIR="."
+
+if [ -f ./___*___*___ ] ; then
+
+  # An empty file matching above pattern identifies the disk / tree where this listing is launched
+  # Write the listings into ./___listings___/<DISK_ID>/ instead
+
+  disk_id_file=$(echo ___*___*___)
+  disk_id_size=$(stat -c %s "$disk_id_file")
+
+  if [ "$disk_id_size" == 0 ]; then
+    echo
+    echo "Disk ID file found: \"$disk_id_file\""
+    BASEDIR="./___listings___/$disk_id_file/$TS"
+    echo "  All listing files will be written into outout dir $BASEDIR"
+    echo "  Note that paths are not altered, they kept relative to ."
+    echo
+    mkdir -p "$BASEDIR"
+  else
+    echo "ERROR. Non-empty disk ID file candidate \"$disk_id_file\" ($disk_id_size bytes) found. Disk ID file is expected to be empty, exiting"
+    exit 1
+  fi
+fi
+
+LOF="${BASEDIR}/all-${TS}.filelist"
+SOF="${BASEDIR}/all-${TS}.sha1"
+LSR="${BASEDIR}/all-${TS}.lsr"
+DUF="${BASEDIR}/all-${TS}.du"
+GRS="${BASEDIR}/all-${TS}.gitrepos"
+
+
+echo "Create full file list using base dir $BASEDIR"
+echo
+echo "  List of files:                         $LOF"
+echo "  All SHA1 sums:                         $SOF"
+echo "  ls -lAR --time-style=full-iso listing: $LSR"
+echo "  du report:                             $DUF"
+echo "  collection of git remotes:             $GRS"
 echo
 
 CT=0
@@ -53,9 +93,11 @@ while read line ; do
 
 done < <(cat "$LOF" | grep -a "\.sha1$")
 
-echo "Collect DU report"
+echo
+echo "Collect directory DU report"
 du --apparent-size -b > "$DUF"
 
+echo
 echo "Collect git repos into $GRS"
 
 
@@ -65,6 +107,8 @@ find -type d -name '.git' | while read dir; do
 done | tee -a "$GRS"
 
 
+echo
+echo "Calculate SHA1 sum of listing files "
 sha1sum -b "$LOF" >> "$SOF"
 sha1sum -b "$LSR" >> "$SOF"
 sha1sum -b "$DUF" >> "$SOF"
@@ -76,10 +120,10 @@ echo
 echo
 echo "All done, stats:"
 echo
-echo
-echo "Number of lines in $SOF (all checksums, might contain duplicates): "$(wc -l < "$SOF")
-echo "Number of lines in $LSR (recursive directory list): "$(wc -l < "$LSR")
-echo "Number of lines in $LOF (all files listed): "$(wc -l < "$LOF")
-echo "Number of lines in $DUF (DU report): $(wc -l < "$DUF")"
+echo "  files list: $(wc -l < "$LOF") lines ($LOF)"
+echo "  recursive ls -R: $(wc -l < "$LSR") lines ($LSR)"
+echo "  du report: $(wc -l < "$DUF") lines ($DUF)"
+echo "  sha1 checksums: $(wc -l < "$SOF") lines ($SOF)"
+echo "  git repo remotes: $(wc -l < "$GRS") lines ($GRS)"
 echo
 echo
